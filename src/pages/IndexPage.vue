5398667aace6e23bb5accc7acf29c133f2351c04
Scott Molinari <scott.molinari@adduco.de>
2023-05-11 06:13:58 +0200

    feat: add files to debug with

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { add } from './test.js'
+debugger
+export default defineComponent({
+  name: 'IndexPage'
+})
+const c = add(1, 7)
+console.log(c)
+
+</script>
